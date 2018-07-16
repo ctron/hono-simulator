6b3230a02541e45f8be0e0bd8e5f0b8fd9cd7172
Jens Reimann <jreimann@redhat.com>
2018-07-16 14:01:07 +0200

    Better cache URL instances

--- simulator-http/src/main/java/de/dentrassi/hono/simulator/http/provider/JavaDevice.java
+++ simulator-http/src/main/java/de/dentrassi/hono/simulator/http/provider/JavaDevice.java
@@ -13,13 +13,13 @@ package de.dentrassi.hono.simulator.http.provider;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import de.dentrassi.hono.demo.common.Register;
 import de.dentrassi.hono.simulator.http.Device;
 import de.dentrassi.hono.simulator.http.Statistics;
 import io.glutamate.lang.ThrowingConsumer;
-import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 
 public class JavaDevice extends Device {
@@ -33,17 +33,22 @@ public class JavaDevice extends Device {
     }
 
     private final byte[] payload;
+    private final URL telemetryUrl;
+    private final URL eventUrl;
 
     public JavaDevice(final String user, final String deviceId, final String tenant, final String password,
             final OkHttpClient client, final Register register, final Statistics telemetryStatistics,
             final Statistics eventStatistics) {
         super(user, deviceId, tenant, password, register, telemetryStatistics, eventStatistics);
         this.payload = "{foo:42}".getBytes(StandardCharsets.UTF_8);
+
+        this.telemetryUrl = createUrl("telemetry").url();
+        this.eventUrl = createUrl("event").url();
     }
 
-    protected void process(final Statistics statistics, final HttpUrl url) throws IOException {
+    protected void process(final Statistics statistics, final URL url) throws IOException {
 
-        final HttpURLConnection con = (HttpURLConnection) url.url().openConnection();
+        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
         try {
             con.setDoInput(false);
             con.setDoOutput(true);
@@ -78,14 +83,12 @@ public class JavaDevice extends Device {
 
     @Override
     protected ThrowingConsumer<Statistics> tickTelemetryProvider() {
-        final HttpUrl url = createUrl("telemetry");
-        return s -> process(s, url);
+        return s -> process(s, this.telemetryUrl);
     }
 
     @Override
     protected ThrowingConsumer<Statistics> tickEventProvider() {
-        final HttpUrl url = createUrl("event");
-        return s -> process(s, url);
+        return s -> process(s, this.eventUrl);
     }
 
 }
