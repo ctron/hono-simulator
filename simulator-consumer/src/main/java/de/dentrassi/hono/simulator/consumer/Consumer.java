1ddcadf2c2f8ae892c59e96de1166a17bfd0a7ee
Jens Reimann <jreimann@redhat.com>
2019-01-18 11:27:11 +0100

    Add missing license header

--- simulator-consumer/src/main/java/de/dentrassi/hono/simulator/consumer/Consumer.java
+++ simulator-consumer/src/main/java/de/dentrassi/hono/simulator/consumer/Consumer.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2018 Red Hat Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jens Reimann - initial API and implementation
+ *******************************************************************************/
+
 package de.dentrassi.hono.simulator.consumer;
 
 import org.apache.qpid.proton.message.Message;
